a84acd6175beb5c8fac207f6f6c05c92a7d50d1b
Shreya Maurya <142162616+shreya38@users.noreply.github.com>
2024-05-11 10:32:47 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/shreya38/AI-Summarizer", "_blank")
           }
           className='black_btn'
         >
